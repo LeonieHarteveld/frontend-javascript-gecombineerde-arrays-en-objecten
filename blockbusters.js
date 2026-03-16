ac5b1b854395c4e1a81979daf53b297a45f00d50
leharteveld <l.e.harteveld@gmail.com>
2026-03-16 12:04:56 +0100

    Huiswerk afgerond

--- blockbusters.js
+++ blockbusters.js
@@ -1,40 +1,40 @@
 const movies = [
-  {
-    title: "The Shawshank Redemption",
-    releaseDate: 1994,
-    rating: 9.2,
-    actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"]
-  },
-  {
-    title: "The Godfather",
-    releaseDate: 1972,
-    rating: 9.2,
-    actors: ["Marlon Brando", "Al Pacino", "James Caan"]
-  },
-  {
-    title: "The Dark Knight",
-    releaseDate: 2008,
-    rating: 8.5,
-    actors: ["Christian Bale", "Heath Ledger", "Gary Oldman"]
-  },
-  {
-    title: "Pulp Fiction",
-    releaseDate: 1994,
-    rating: 7.8,
-    actors: ["John Travolta", "Samuel L. Jackson", "Uma Thurman"]
-  },
-  {
-    title: "The Lord of the Rings: The Return of the King",
-    releaseDate: 2003,
-    rating: 8.9,
-    actors: ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"]
-  },
-  {
-    title: "12 Angry Men",
-    releaseDate: 1957,
-    rating: 6.2,
-    actors: ["Henry Fonda", "Lee J. Cobb", "Martin Balsam"]
-  }
+    {
+        title: "The Shawshank Redemption",
+        releaseDate: 1994,
+        rating: 9.2,
+        actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"]
+    },
+    {
+        title: "The Godfather",
+        releaseDate: 1972,
+        rating: 9.2,
+        actors: ["Marlon Brando", "Al Pacino", "James Caan"]
+    },
+    {
+        title: "The Dark Knight",
+        releaseDate: 2008,
+        rating: 8.5,
+        actors: ["Christian Bale", "Heath Ledger", "Gary Oldman"]
+    },
+    {
+        title: "Pulp Fiction",
+        releaseDate: 1994,
+        rating: 7.8,
+        actors: ["John Travolta", "Samuel L. Jackson", "Uma Thurman"]
+    },
+    {
+        title: "The Lord of the Rings: The Return of the King",
+        releaseDate: 2003,
+        rating: 8.9,
+        actors: ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"]
+    },
+    {
+        title: "12 Angry Men",
+        releaseDate: 1957,
+        rating: 5.2,
+        actors: ["Henry Fonda", "Lee J. Cobb", "Martin Balsam"]
+    }
 ];
 
 // ------------------------------------------------------------------------------ //
@@ -48,13 +48,14 @@ const movies = [
 //     actors: [ "Tim Robbins", "Morgan Freeman", "Bob Gunton" ]
 // }
 
-
+console.log(movies[0]);
 
 // Opdracht 1b: Log de titel van het eerste object in de 'movies' array
 
 // Verwacht antwoord:
 // The Shawshank Redemption
 
+console.log(movies[0].title);
 
 // ------------------------------------------------------------------------------ //
 // Opdracht 2a: Log de hoeveel films er in de 'movies' array staan (de lengte van de array). Let op: wanneer de array langer of korter wordt, moet dit nog steeds werken. Je mag dus niet zelf tellen!
@@ -62,6 +63,9 @@ const movies = [
 // Verwacht antwoord:
 // 6
 
+const numberMovies = movies.length
+
+console.log("Er zijn " + numberMovies + " films aanwezig");
 
 
 // Opdracht 2b: Log het indexnummer van de laatste film in de 'movies' array. Let op: wanneer de array langer of korter wordt, moet dit nog steeds werken!
@@ -69,7 +73,9 @@ const movies = [
 // Verwacht antwoord:
 // 5
 
+lastMovie = numberMovies - 1
 
+console.log(lastMovie)
 
 // Opdracht 2c: Log het laatste object in de 'movies' array. Let op: wanneer de array langer of korter wordt, moet dit nog steeds werken!
 // Tip: maak gebruik van de code die je in de vorige opdracht hebt gemaakt.
@@ -82,7 +88,7 @@ const movies = [
 //     actors: [ 'Henry Fonda', 'Lee J. Cobb', 'Martin Balsam' ]
 // }
 
-
+console.log(movies[lastMovie]);
 
 // ------------------------------------------------------------------------------ //
 // Opdracht 3a: Log de lijst van acteurs uit van de film "Pulp Fiction" uit de 'movies' array.
@@ -90,13 +96,14 @@ const movies = [
 // Verwachte antwoord:
 // ["John Travolta", "Samuel L. Jackson", "Uma Thurman"]
 
-
+console.log(movies[3].actors)
 
 // Opdracht 3b: Log de tweede acteur van de film "The Shawshank Redemption". Tip: je spreekt dus eerst de array, daarna het object en vervolgens weer een array aan.
 
 // Verwacht antwoord:
 // Morgan Freeman
 
+console.log(movies[0].actors[1]);
 
 
 // Opdracht 3c: Log de laatste acteur van de laatste film in 'movies' array. Let op: wanneer de array langer of korter wordt, of wanneer er meer of minder acteurs in dit rijtje staan, moet dit nog steeds werken!
@@ -105,7 +112,9 @@ const movies = [
 // Verwacht antwoord:
 // Martin Balsam
 
+lastActor = movies[lastMovie].actors.length - 1;
 
+console.log(movies[lastMovie].actors[lastActor]);
 
 // ------------------------------------------------------------------------------ //
 // Opdracht 4a: Maak een if-else-statement die het woord 'Uitstekende film!!' logt,
@@ -121,7 +130,11 @@ const selectedMovie = movies[0];
 // Verwacht antwoord bij 'const selectedMovie = movies[3]':
 // Goede film!
 
-
+if (selectedMovie.rating > 8) {
+    console.log("Uitstekende film!!");
+} else {
+    console.log("Goede film!");
+}
 
 // Opdracht 4b: Maak een if-else-if-else-statement die het woord 'Topper!' logt,
 // wanneer een film een hoger cijfer krijgt dan een 8 en het woord 'Kijktip!' logt, wanneer deze een 8 of lager is,
@@ -138,3 +151,10 @@ const selectedMovie = movies[0];
 // Verwacht antwoord bij 'const selectedMovie = movies[5]':
 // Matige film
 
+if (selectedMovie.rating > 8) {
+    console.log("Topper!");
+} else if (selectedMovie.rating <= 8 && selectedMovie.rating > 6) {
+    console.log("Kijktip!")
+} else if (selectedMovie.rating <= 6) {
+    console.log("Matige film!")
+}
